952850ae1340f655ee895634f6f9d51d02c059b5
Logan Hart <2loganhart@gmail.com>
2018-07-27 19:17:04 -0700

    add bcrypt-nodejs to project, encrypt a new user's password on save hook

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -2,6 +2,9 @@ const User = require('../models/user');
 
 exports.signup = (req, res, next) => {
   const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(422).send({ error: 'You must provide email and password' });
+  }
   User.findOne({ email: email })
     .then((existingUser) => {
       if (existingUser) {
--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,23 @@
 const { mongoose } = require('../config/db-config');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String
+});
+
+// on save hook, encrypt password
+userSchema.pre('save', function(next) {
+  const user = this;
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) { return next(err); }
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) { return next(err); }
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 const ModelClass = mongoose.model('user', userSchema);
