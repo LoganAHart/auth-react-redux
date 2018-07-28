c2e2563c18ad75d792cca54d65a34a199d96c483
Logan Hart <2loganhart@gmail.com>
2018-07-27 18:44:47 -0700

    Implement signup route, creates a new user and saves to db, refactor project to export mongoose connection from db-config in order to replace mongoose.Promise with global.Promise

---
+++ server/controllers/authentication.js
@@ -0,0 +1,25 @@
+const User = require('../models/user');
+
+exports.signup = (req, res, next) => {
+  const { email, password } = req.body;
+  User.findOne({ email: email })
+    .then((existingUser) => {
+      if (existingUser) {
+        throw new Error('Email is in use');
+      }
+      const user = new User({
+        email: email,
+        password: password,
+      });
+      return user.save();
+    })
+    .then(() => {
+      return res.json({ success: true });
+    })
+    .catch((err) => {
+      if (err.message === 'Email is in use') {
+        return res.status(422).send({ error: err.message });
+      }
+      return next(err);
+    });
+}
--- server/index.js
+++ server/index.js
@@ -2,13 +2,9 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const mongoose = require('mongoose');
 const app = express();
 const router = require('./router');
-
-
-// DB Setup
-mongoose.connect('mongodb://localhost:27017/auth', { useNewUrlParser: true });
+const { mongoose } = require('./config/db-config');
 
 // App Setup
 app.use(morgan('combined'));
